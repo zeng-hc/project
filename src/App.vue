89779c9fc40d4153840d10ba42b23d9f69830f84
匆聪忙忙 <1310488502@qq.com>
2021-07-07 15:23:39 +0800

    xxx

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+[v-cloak]{
+  display: none;
+}
+*{
+  margin: 0;
+  padding: 0;
+}
+body{
+  background-color: #eee;
+  overflow-y: hidden;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
